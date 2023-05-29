bfb065bf41c0d20b6c9912ae1470ffb86126b844
Eudes Serpa <serpagame@gmail.com>
2023-05-29 16:24:09 -0500

    feat(i18n about): add i18n to the about section

--- src/i18n/locales/en/translation.ts
+++ src/i18n/locales/en/translation.ts
@@ -22,20 +22,51 @@ const HERO = {
 	"hero.introduction": "Hi, there! I'm",
 	"hero.professions.ing": "Electronic engineer",
 	"hero.professions.dev": "Web developer",
-	"hero.description": "I am passionate about technology and programming. I have experience in the development of embedded systems, which act as a bridge between software and hardware, as well as in web development using the JavaScript ecosystem.",
+	"hero.description":
+		"I am passionate about technology and programming. I have experience in the development of embedded systems, which act as a bridge between software and hardware, as well as in web development using the JavaScript ecosystem.",
 	"hero.cta.contact": "Contact Me",
 	"hero.cta.see-cv": "See CV",
 	"hero.process.title": "My learning process",
 	"hero.process.study": "Study",
 	"hero.process.practice": "Practice",
 	"hero.process.build": "Build",
-}
+};
+
+const ABOUT = {
+	"about.title": "About me",
+	"about.whoiam.title": "Who I am?",
+	"about.whoiam.description": `
+	<0>
+	I'm a young Colombian graduated in Electronic Engineering from the University
+	of Magdalena and passionate about software development. Currently, I am
+	studying a technologist in the area of Software Analysis and Development at
+	the National Learning Service, SENA (for its acronym in Spanish).
+	</0>
+	<1>
+	My first approach to programming and software development was during my first
+	semester of undergraduate, where I had the opportunity to familiarize myself
+	with the C programming language and the Arduino microcontroller. Although it
+	was a challenging start, I discovered a genuine passion for these areas.
+	</1>
+	<2>
+	Over the past few years, I have invested countless hours in studying
+	and practicing various programming languages,	frameworks and web
+	technologies. This dedication has allowed me to acquire solid
+	knowledge in web development and embedded systems.
+	</2>
+	<3>
+	If you're looking for a committed, self-taught and enthusiastic professional
+	who can bring a unique perspective and solid technical skills to your team,
+	I'd love the opportunity to discuss how I can contribute to your project!
+	</3>`,
+	"about.skills.title": "Skills",
+};
 
 const FOOTER = {
 	"footer.credits.text": "Designed & developed with",
 	"footer.credits.aux": "love",
 	"footer.credits.by": "by",
-}
+};
 
 const I11N = {
 	"i11n.theme.dark.label": "Toggles to dark mode",
@@ -44,10 +75,10 @@ const I11N = {
 	"i11n.menu.expanded.label": "Close menu",
 	"i11n.menu.no.expanded.label": "Open menu",
 
-  "i11n.logo.label": "Go to home",
+	"i11n.logo.label": "Go to home",
 
 	"i11n.hero.btn-slide.label.start": "Start automatic slide show",
-	"i11n.hero.btn-slide.label.pause": "Stop automatic slide show"
+	"i11n.hero.btn-slide.label.pause": "Stop automatic slide show",
 };
 
 export const enTranslations = {
@@ -57,5 +88,6 @@ export const enTranslations = {
 	...SKIP_LINK,
 	...I11N,
 	...HERO,
+	...ABOUT,
 	...FOOTER,
 };
--- src/i18n/locales/es/translation.ts
+++ src/i18n/locales/es/translation.ts
@@ -22,20 +22,53 @@ const HERO = {
 	"hero.introduction": "¡Hola!, soy",
 	"hero.professions.ing": "Ingeniero electrónico",
 	"hero.professions.dev": "Desarrollador web",
-	"hero.description": "Soy un apasionado de la tecnología y la programación. Tengo experiencia en el desarrollo de sistemas embebidos, que actúan como puente entre el software y el hardware, así como en el desarrollo web utilizando el ecosistema de JavaScript.",
+	"hero.description":
+		"Soy un apasionado de la tecnología y la programación. Tengo experiencia en el desarrollo de sistemas embebidos, que actúan como puente entre el software y el hardware, así como en el desarrollo web utilizando el ecosistema de JavaScript.",
 	"hero.cta.contact": "Contáctame",
 	"hero.cta.see-cv": "Ver CV",
 	"hero.process.title": "Mi proceso de aprendizaje",
 	"hero.process.study": "Estudiar",
 	"hero.process.practice": "Pracicar",
 	"hero.process.build": "Construir",
-}
+};
+
+const ABOUT = {
+	"about.title": "Sobre mí",
+	"about.whoiam.title": "¿Quién soy?",
+	"about.whoiam.description": `
+	<0>
+	Soy un joven colombiano graduado en Ingeniería Electrónica de la Universidad
+	del Magdalena y apasionado por el desarrollo de software.	Actualmente, estoy
+	cursando un tecnólogo en el área de Análisis y Desarrollo de Software en el
+	Servicio Nacional de Aprendizaje, SENA.
+	</0>
+	<1>
+	Mi primer acercamiento a la programación y al desarrollo de software
+	ocurrió durante mi primer semestre de pregrado, donde tuve la
+	oportunidad de familiarizarme con el lenguaje de programación C y el
+	microcontrolador Arduino. Aunque fue un inicio desafiante, descubrí
+	una pasión genuina por estas áreas.
+	</1>
+	<2>
+	A lo largo de los últimos años, he invertido innumerables horas en
+	estudiar y practicar diversos lenguajes de programación, frameworks y
+	tecnologías web. Esta dedicación me ha permitido adquirir
+	conocimientos sólidos en desarrollo web y sistemas embebidos.
+	</2>
+	<3>
+	Si estás en busca de un profesional comprometido, autodidacta y
+	entusiasta que pueda aportar una perspectiva única y habilidades
+	técnicas sólidas a tu equipo, ¡me encantaría tener la oportunidad de
+	discutir cómo puedo contribuir a tu proyecto!
+	</3>`,
+	"about.skills.title": "Habilidades",
+};
 
 const FOOTER = {
 	"footer.credits.text": "Diseñado y desarrollado con",
 	"footer.credits.aux": "amor",
 	"footer.credits.by": "por",
-}
+};
 
 const I11N = {
 	"i11n.theme.dark.label": "Cambiar a modo oscuro",
@@ -47,7 +80,7 @@ const I11N = {
 	"i11n.logo.label": "Ir al inicio",
 
 	"i11n.hero.btn-slide.label.start": "Iniciar presentación automática",
-	"i11n.hero.btn-slide.label.pause": "Detener presentación automática"
+	"i11n.hero.btn-slide.label.pause": "Detener presentación automática",
 };
 
 export const esTranslations = {
@@ -57,5 +90,6 @@ export const esTranslations = {
 	...SKIP_LINK,
 	...I11N,
 	...HERO,
+	...ABOUT,
 	...FOOTER,
 };
