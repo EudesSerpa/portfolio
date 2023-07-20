b4c575fbd275bec6c59b2e9db38fa9a2e7ab10bb
Eudes Serpa <serpagame@gmail.com>
2023-07-20 00:17:18 -0500

    feat(contact): add contact section assets

--- src/utils/const.ts
+++ src/utils/const.ts
@@ -1,6 +1,7 @@
 export const AUTHOR = {
 	name: "Eudes",
 	lastName: "Serpa",
+	email: "eudeserpa@gmail.com",
 	github: "https://github.com/EudesSerpa",
 	linkedin: "https://linkedin.com/in/eudes-serpa",
 } as const;
